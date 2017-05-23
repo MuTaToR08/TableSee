4b78186e9e91dd00f766fe7cc4286055fc438f7d
Усенков Иван <mutator08@mail.ru>
2017-05-23 07:42:16 +0300

    Упорядоченная таблица

--- TableSee/TableSee.h
+++ TableSee/TableSee.h
@@ -7,7 +7,7 @@ class table
 private:
 	K* arrKey;
 	V* arrVal;
-	size_t MaxSizeTable,countUsed;
+	size_t MaxSizeTable, countUsed;
 
 public:
 	table(size_t size);
@@ -25,11 +25,20 @@ public:
 	void resize(size_t newsized);
 
 
+	friend std::ostream& operator<<(std::ostream& os, table<K, V>& t)
+	{
+		for (size_t i = 0; i < t.countUsed; i++)
+		{
+			os << t.arrKey[i] << " | " << t.arrVal[i] << std::endl;
+		}
+
+		return os;
+	}
 
 };
 
 template<class K, class V>
-table<K,V>::table(size_t size)
+table<K, V>::table(size_t size)
 {
 	MaxSizeTable = size;
 	arrKey = new K[size];
@@ -53,12 +62,23 @@ void table<K, V>::add(K k, V v)
 {
 	for (size_t i = 0; i < MaxSizeTable; i++)
 	{
-		if (arrKey[i] == NULL)
+		if (arrKey[i] == NULL || arrKey[i] >= k)
 		{
-			arrKey[i] = k;
-			arrVal[i] = v;
-			countUsed++;
+			arrKey[countUsed] = k;
+			arrVal[countUsed++] = v;
+
+			for (size_t x = i; i < countUsed - 1; i++)
+			{
+				K tk = arrKey[x];
+				V tv = arrVal[x];
+
+				arrKey[x] = arrKey[countUsed - 1];
+				arrVal[x] = arrVal[countUsed - 1];
+				arrKey[countUsed - 1] = tk;
+				arrVal[countUsed - 1] = tv;
+			}
 			return;
+
 		}
 	}
 
@@ -73,8 +93,12 @@ void table<K, V>::remove(K k)
 	{
 		if (arrKey[i] == k)
 		{
-			countUsed--;
-			arrKey[i] = NULL;
+			for (size_t x = i; i < countUsed; i++)
+			{
+				arrKey[x] = arrKey[countUsed - 1];
+				arrVal[x] = arrVal[countUsed - 1];
+			}
+			arrKey[countUsed--] = NULL;
 			break;
 		}
 	}
@@ -96,7 +120,7 @@ V & table<K, V>::operator[](K k)
 template<class K, class V>
 
 
-table<K,V> & table<K, V>::operator=(table & vt)
+table<K, V> & table<K, V>::operator=(table & vt)
 {
 	MaxSizeTable = vt.MaxSizeTable;
 	arrKey = new K[MaxSizeTable];
@@ -114,25 +138,24 @@ table<K,V> & table<K, V>::operator=(table & vt)
 template<class K, class V>
 void table<K, V>::resize(size_t newsized)
 {
-		if (newsized == MaxSizeTable)
-			return;
-		if (newsized < countUsed)
-			throw string("Need clear table");
+	if (newsized == MaxSizeTable)
+		return;
+	if (newsized < countUsed)
+		throw string("Need clear table");
 
-		K* Tkey = new K[newsized];
-		V* Tval = new V[newsized];
-		size_t insertindex = 0;
-		for (int i = 0; i < MaxSizeTable; i++)
-		{
-			if (arrKey[i] != NULL)
-			{
-				Tkey[insertindex] = arrKey[i];
-				Tval[insertindex] = arrVal[i];
-				++insertindex;
-			}
-		}
-		delete[]arrKey, arrVal;
-		arrKey = Tkey;
-		arrVal = Tval;
-		MaxSizeTable = newsized;
+	K* Tkey = new K[newsized];
+	V* Tval = new V[newsized];
+	size_t insertindex = 0;
+	for (int i = 0; i < MaxSizeTable; i++)
+	{
+		Tkey[insertindex] = arrKey[i];
+		Tval[insertindex] = arrVal[i];
+		++insertindex;
+		if (arrKey[i] == NULL)
+			break;
+	}
+	delete[]arrKey, arrVal;
+	arrKey = Tkey;
+	arrVal = Tval;
+	MaxSizeTable = newsized;
 }
